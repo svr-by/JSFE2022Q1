a38169b7c540c146dd6a4ae3bdfc171f134d6fda
Siarhei <siarhei.by@gmail.com>
2022-04-20 23:20:32 +0300

    feat: add burger menu

---
+++ shelter/pages/main/scripts.js
@@ -0,0 +1,34 @@
+"use strict"
+// Burger menu
+const BURGER = document.querySelector('.burger');
+const LOGO = document.querySelector('.header__logo');
+const NAV = document.querySelector('.header__nav');
+const BACK = document.querySelector('.back');
+
+function toggleMenu () {
+    BURGER.classList.toggle('open');
+    NAV.classList.toggle('open');
+    LOGO.classList.toggle('hidden');
+    BACK.classList.toggle('visible');
+    document.body.classList.toggle('noscroll');
+}
+
+function closeMenu() {
+    BURGER.classList.remove('open');
+    NAV.classList.remove('open');
+    LOGO.classList.remove('hidden');
+    BACK.classList.remove('visible');
+    document.body.classList.remove('noscroll');
+}
+
+window.addEventListener('resize', closeMenu);
+BURGER.addEventListener('click', toggleMenu);
+BACK.addEventListener('click', closeMenu);
+NAV.addEventListener('click', (event) => {if(event.target.classList.contains('nav__link')) closeMenu()});
+
+
+const PETS = [];
+fetch("../../js/pets.json")
+.then(response => response.json())
+.then(data => data.forEach(pet => PETS.push(pet)));
+
