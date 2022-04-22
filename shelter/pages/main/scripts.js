a8583136eadfe416821f48b46e7e1e49333ceaec
Siarhei <siarhei.by@gmail.com>
2022-04-22 20:23:21 +0300

    feat: add slider

--- shelter/pages/main/scripts.js
+++ shelter/pages/main/scripts.js
@@ -78,3 +78,47 @@ function removePopup(event) {
 
 petCards.forEach(card => card.querySelector('.button').addEventListener('click', createPopup));
 
+//Slider
+
+let sliderBtn = document.querySelectorAll('.slider__btn');
+let sliderContainer = document.querySelector('.slider__container');
+let currentSlideIndex = [4, 0, 2];
+
+sliderBtn.forEach(btn => btn.addEventListener('click', showNextSlide));
+
+function showNextSlide() {
+	sliderContainer.innerHTML = '';
+	let nextSlideIndex = randimizeCard();
+	createSlide(nextSlideIndex, sliderContainer);
+}
+
+function randimizeCard() {
+    let nextArr = [];
+    while (nextArr.length < currentSlideIndex.length) {
+        let randomIndex = Math.floor(Math.random() * 8);
+        if (!currentSlideIndex.includes(randomIndex) && !nextArr.includes(randomIndex)) {
+            nextArr.push(randomIndex)
+        }
+    }
+    currentSlideIndex = [...nextArr];
+	return nextArr;
+};
+
+function createSlide(arr, container){
+
+	for(let i=0; i < arr.length; i++){
+		let petObj = PETS[arr[i]];
+        console.log(arr[i], petObj.name);
+		let petCard = document.createElement("div");
+		petCard.className = "pets__card";
+		petCard.innerHTML = `
+			<div class="card__img">
+				<img src=${petObj.img || ""} alt=${petObj.name || ""} class="image">
+			</div>
+			<p class="card__name">${petObj.name || ""}</p>
+			<button class="button btn__clear">Learn more</button>
+		`;
+        petCard.querySelector('.button').addEventListener('click', createPopup);
+		container.append(petCard);
+	}
+}
