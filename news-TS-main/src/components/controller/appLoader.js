8094f9545357124850ddb4f31b6a1e380a584b1c
Siarhei <siarhei.by@gmail.com>
2022-07-01 19:51:00 +0300

    feat: add key for newsapi.org

--- news-TS-main/src/components/controller/appLoader.js
+++ news-TS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bba3a4b205b04fac85cd330751b08cbb',
         });
     }
 }
