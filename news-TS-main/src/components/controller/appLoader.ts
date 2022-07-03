773e20f2e99f86385d89b7ba419ebe1ad5078934
Siarhei <siarhei.by@gmail.com>
2022-07-03 20:46:19 +0300

    feat: add deployment files

--- news-TS-main/src/components/controller/appLoader.ts
+++ news-TS-main/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'c81c583ce72c4eb68ecb94dc0a5ddaa1',
         });
     }
