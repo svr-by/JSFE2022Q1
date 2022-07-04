420a425f703174b8d7d6499e05cb2c4de829edbe
Siarhei <siarhei.by@gmail.com>
2022-07-04 20:26:23 +0300

    feat: implement stack

--- data-structures/stack.ts
+++ data-structures/stack.ts
@@ -1,17 +1,26 @@
 // Read about stack and learn its working mechanism and implement it functionality.
 
-class Stack {
-	push() {
-		// set value to stack.
+class Stack<T> {
+    protected _stack: T[];
+
+    constructor () { 
+        this._stack = [];
+    }
+
+	// set value to stack.
+	push(el: T) { 
+		this._stack.push(el);
 	}
 
-	pop() {
-		// get value from stack and remove it
+	// get value from stack and remove it
+	pop() { 
+		return this._stack.pop();
 	}
 
-	isEmpty() {
-		// should return true when stack is empty, otherwise false
+	// should return true when stack is empty, otherwise false
+	isEmpty() { 
+		return (this._stack.length === 0);
 	}
 }
 
-const stack = new Stack();
+const stack = new Stack<number>();
