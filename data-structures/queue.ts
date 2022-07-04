9c634ff50003463c41691cd1e282d0a4ca4b9a52
Siarhei <siarhei.by@gmail.com>
2022-07-04 20:48:06 +0300

    feat: implement queue

--- data-structures/queue.ts
+++ data-structures/queue.ts
@@ -1,17 +1,26 @@
 // Read about queue and learn its working mechanism and implement it functionality.
 
-class Queue {
-	enqueue() {
-		// set value to queue
-	}
+class Queue<T> {
+    protected _queue: T[];
 
-	dequeue() {
-		// remove value from queue
-	}
+    constructor() {
+        this._queue = [];
+    }
 
-	isEmpty() {
-		// should return true when queue is empty, otherwise false
-	}
+    // set value to queue
+    enqueue(el: T) {
+        this._queue.push(el);
+    }
+
+    // remove value from queue
+    dequeue() {
+        return this._queue.shift();
+    }
+
+    // should return true when queue is empty, otherwise false
+    isEmpty() {
+        return (this._queue.length === 0);
+    }
 }
 
-const queue = new Queue();
+const queue = new Queue<number>();
